359fd83024e7a8ff3c5ce15d4d0f3769c0ae9929
twinaphex <libretro@gmail.com>
2015-08-12 18:48:25 +0200

    Use MSB_FIRST everywhere for big endian codepaths

--- libpcsxcore/misc.c
+++ libpcsxcore/misc.c
@@ -59,7 +59,7 @@ void mmssdd( char *b, char *p )
 #if defined(__arm__)
 	unsigned char *u = (void *)b;
 	int block = (u[3] << 24) | (u[2] << 16) | (u[1] << 8) | u[0];
-#elif defined(__BIGENDIAN__)
+#elif defined(MSB_FIRST)
 	int block = (b[0] & 0xff) | ((b[1] & 0xff) << 8) | ((b[2] & 0xff) << 16) | (b[3] << 24);
 #else
 	int block = *((int*)b);
