c941d0208ec14ce0ea384efeba45f56970997506
twinaphex <libretro@gmail.com>
2015-08-12 18:59:49 +0200

    (psxmem.c) Cleanups

--- libpcsxcore/psxmem.c
+++ libpcsxcore/psxmem.c
@@ -210,7 +210,8 @@ void psxMemReset() {
 	} else Config.HLE = TRUE;
 }
 
-void psxMemShutdown() {
+void psxMemShutdown()
+{
 	psxUnmap(psxM, 0x00210000, MAP_TAG_RAM); psxM = NULL;
 	psxUnmap(psxH, 0x10000, MAP_TAG_OTHER); psxH = NULL;
 	psxUnmap(psxR, 0x80000, MAP_TAG_OTHER); psxR = NULL;
@@ -222,78 +223,83 @@ void psxMemShutdown() {
 static int writeok = 1;
 
 u8 psxMemRead8(u32 mem) {
-	char *p;
-	u32 t;
+   char *p;
+   u32 t = mem >> 16;
 
-	t = mem >> 16;
-	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80) {
-		if ((mem & 0xffff) < 0x400)
-			return psxHu8(mem);
-		else
-			return psxHwRead8(mem);
-	} else {
-		p = (char *)(psxMemRLUT[t]);
-		if (p != NULL) {
-			if (Config.Debug)
-				DebugCheckBP((mem & 0xffffff) | 0x80000000, R1);
-			return *(u8 *)(p + (mem & 0xffff));
-		} else {
-#ifdef PSXMEM_LOG
-			PSXMEM_LOG("err lb %8.8lx\n", mem);
+   if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80)
+   {
+      if ((mem & 0xffff) < 0x400)
+         return psxHu8(mem);
+      return psxHwRead8(mem);
+   }
+   p = (char *)(psxMemRLUT[t]);
+   if (p != NULL)
+   {
+#ifdef DEBUG
+      if (Config.Debug)
+         DebugCheckBP((mem & 0xffffff) | 0x80000000, R1);
 #endif
-			return 0;
-		}
-	}
+      return *(u8 *)(p + (mem & 0xffff));
+   }
+
+#ifdef PSXMEM_LOG
+   PSXMEM_LOG("err lb %8.8lx\n", mem);
+#endif
+   return 0;
 }
 
-u16 psxMemRead16(u32 mem) {
-	char *p;
-	u32 t;
+u16 psxMemRead16(u32 mem)
+{
+   char *p;
+   u32   t = mem >> 16;
 
-	t = mem >> 16;
-	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80) {
-		if ((mem & 0xffff) < 0x400)
-			return psxHu16(mem);
-		else
-			return psxHwRead16(mem);
-	} else {
-		p = (char *)(psxMemRLUT[t]);
-		if (p != NULL) {
-			if (Config.Debug)
-				DebugCheckBP((mem & 0xffffff) | 0x80000000, R2);
-			return SWAPu16(*(u16 *)(p + (mem & 0xffff)));
-		} else {
-#ifdef PSXMEM_LOG
-			PSXMEM_LOG("err lh %8.8lx\n", mem);
+   if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80)
+   {
+      if ((mem & 0xffff) < 0x400)
+         return psxHu16(mem);
+      return psxHwRead16(mem);
+   }
+
+   p = (char *)(psxMemRLUT[t]);
+   if (p != NULL)
+   {
+#ifdef DEBUG
+      if (Config.Debug)
+         DebugCheckBP((mem & 0xffffff) | 0x80000000, R2);
 #endif
-			return 0;
-		}
-	}
+      return SWAPu16(*(u16 *)(p + (mem & 0xffff)));
+   }
+#ifdef PSXMEM_LOG
+   PSXMEM_LOG("err lh %8.8lx\n", mem);
+#endif
+   return 0;
 }
 
-u32 psxMemRead32(u32 mem) {
-	char *p;
-	u32 t;
+u32 psxMemRead32(u32 mem)
+{
+   char *p;
+   u32 t = mem >> 16;
 
-	t = mem >> 16;
-	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80) {
-		if ((mem & 0xffff) < 0x400)
-			return psxHu32(mem);
-		else
-			return psxHwRead32(mem);
-	} else {
-		p = (char *)(psxMemRLUT[t]);
-		if (p != NULL) {
-			if (Config.Debug)
-				DebugCheckBP((mem & 0xffffff) | 0x80000000, R4);
-			return SWAPu32(*(u32 *)(p + (mem & 0xffff)));
-		} else {
-#ifdef PSXMEM_LOG
-			if (writeok) { PSXMEM_LOG("err lw %8.8lx\n", mem); }
+   if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80)
+   {
+      if ((mem & 0xffff) < 0x400)
+         return psxHu32(mem);
+      return psxHwRead32(mem);
+   }
+
+   p = (char *)(psxMemRLUT[t]);
+   if (p != NULL)
+   {
+#ifdef DEBUG
+      if (Config.Debug)
+         DebugCheckBP((mem & 0xffffff) | 0x80000000, R4);
 #endif
-			return 0;
-		}
-	}
+      return SWAPu32(*(u32 *)(p + (mem & 0xffff)));
+   }
+#ifdef PSXMEM_LOG
+   if (writeok) { PSXMEM_LOG("err lw %8.8lx\n", mem); }
+#endif
+   return 0;
 }
 
 void psxMemWrite8(u32 mem, u8 value) {
@@ -309,8 +315,10 @@ void psxMemWrite8(u32 mem, u8 value) {
 	} else {
 		p = (char *)(psxMemWLUT[t]);
 		if (p != NULL) {
+#ifdef DEBUG
 			if (Config.Debug)
 				DebugCheckBP((mem & 0xffffff) | 0x80000000, W1);
+#endif
 			*(u8 *)(p + (mem & 0xffff)) = value;
 #ifdef PSXREC
 			psxCpu->Clear((mem & (~3)), 1);
@@ -333,16 +341,22 @@ void psxMemWrite16(u32 mem, u16 value) {
 			psxHu16ref(mem) = SWAPu16(value);
 		else
 			psxHwWrite16(mem, value);
-	} else {
+	}
+   else
+   {
 		p = (char *)(psxMemWLUT[t]);
 		if (p != NULL) {
+#ifdef DEBUG
 			if (Config.Debug)
 				DebugCheckBP((mem & 0xffffff) | 0x80000000, W2);
+#endif
 			*(u16 *)(p + (mem & 0xffff)) = SWAPu16(value);
 #ifdef PSXREC
 			psxCpu->Clear((mem & (~3)), 1);
 #endif
-		} else {
+		}
+      else
+      {
 #ifdef PSXMEM_LOG
 			PSXMEM_LOG("err sh %8.8lx\n", mem);
 #endif
@@ -356,74 +370,83 @@ void psxMemWrite32(u32 mem, u32 value) {
 
 //	if ((mem&0x1fffff) == 0x71E18 || value == 0x48088800) SysPrintf("t2fix!!\n");
 	t = mem >> 16;
-	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80) {
-		if ((mem & 0xffff) < 0x400)
-			psxHu32ref(mem) = SWAPu32(value);
-		else
-			psxHwWrite32(mem, value);
-	} else {
-		p = (char *)(psxMemWLUT[t]);
-		if (p != NULL) {
-			if (Config.Debug)
-				DebugCheckBP((mem & 0xffffff) | 0x80000000, W4);
-			*(u32 *)(p + (mem & 0xffff)) = SWAPu32(value);
-#ifdef PSXREC
-			psxCpu->Clear(mem, 1);
+	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80)
+   {
+      if ((mem & 0xffff) < 0x400)
+         psxHu32ref(mem) = SWAPu32(value);
+      else
+         psxHwWrite32(mem, value);
+   }
+   else
+   {
+      p = (char *)(psxMemWLUT[t]);
+      if (p != NULL) {
+#ifdef DEBUG
+         if (Config.Debug)
+            DebugCheckBP((mem & 0xffffff) | 0x80000000, W4);
 #endif
-		} else {
-			if (mem != 0xfffe0130) {
+         *(u32 *)(p + (mem & 0xffff)) = SWAPu32(value);
 #ifdef PSXREC
-				if (!writeok)
-					psxCpu->Clear(mem, 1);
+         psxCpu->Clear(mem, 1);
+#endif
+      } else {
+         if (mem != 0xfffe0130) {
+#ifdef PSXREC
+            if (!writeok)
+               psxCpu->Clear(mem, 1);
 #endif
 
 #ifdef PSXMEM_LOG
-				if (writeok) { PSXMEM_LOG("err sw %8.8lx\n", mem); }
+            if (writeok) { PSXMEM_LOG("err sw %8.8lx\n", mem); }
 #endif
-			} else {
-				int i;
+         } else {
+            int i;
 
-				switch (value) {
-					case 0x800: case 0x804:
-						if (writeok == 0) break;
-						writeok = 0;
-						memset(psxMemWLUT + 0x0000, 0, 0x80 * sizeof(void *));
-						memset(psxMemWLUT + 0x8000, 0, 0x80 * sizeof(void *));
-						memset(psxMemWLUT + 0xa000, 0, 0x80 * sizeof(void *));
-						break;
-					case 0x00: case 0x1e988:
-						if (writeok == 1) break;
-						writeok = 1;
-						for (i = 0; i < 0x80; i++) psxMemWLUT[i + 0x0000] = (void *)&psxM[(i & 0x1f) << 16];
-						memcpy(psxMemWLUT + 0x8000, psxMemWLUT, 0x80 * sizeof(void *));
-						memcpy(psxMemWLUT + 0xa000, psxMemWLUT, 0x80 * sizeof(void *));
-						break;
-					default:
+            switch (value)
+            {
+               case 0x800:
+               case 0x804:
+                  if (writeok == 0)
+                     break;
+                  writeok = 0;
+                  memset(psxMemWLUT + 0x0000, 0, 0x80 * sizeof(void *));
+                  memset(psxMemWLUT + 0x8000, 0, 0x80 * sizeof(void *));
+                  memset(psxMemWLUT + 0xa000, 0, 0x80 * sizeof(void *));
+                  break;
+               case 0x00:
+               case 0x1e988:
+                  if (writeok == 1)
+                     break;
+                  writeok = 1;
+                  for (i = 0; i < 0x80; i++) psxMemWLUT[i + 0x0000] = (void *)&psxM[(i & 0x1f) << 16];
+                  memcpy(psxMemWLUT + 0x8000, psxMemWLUT, 0x80 * sizeof(void *));
+                  memcpy(psxMemWLUT + 0xa000, psxMemWLUT, 0x80 * sizeof(void *));
+                  break;
+               default:
 #ifdef PSXMEM_LOG
-						PSXMEM_LOG("unk %8.8lx = %x\n", mem, value);
+                  PSXMEM_LOG("unk %8.8lx = %x\n", mem, value);
 #endif
-						break;
-				}
-			}
-		}
-	}
+                  break;
+            }
+         }
+      }
+   }
 }
 
-void *psxMemPointer(u32 mem) {
-	char *p;
-	u32 t;
+void *psxMemPointer(u32 mem)
+{
+   char *p;
+   u32 t = mem >> 16;
 
-	t = mem >> 16;
-	if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80) {
-		if ((mem & 0xffff) < 0x400)
-			return (void *)&psxH[mem];
-		else
-			return NULL;
-	} else {
-		p = (char *)(psxMemWLUT[t]);
-		if (p != NULL) {
-			return (void *)(p + (mem & 0xffff));
-		}
-		return NULL;
-	}
+   if (t == 0x1f80 || t == 0x9f80 || t == 0xbf80)
+   {
+      if ((mem & 0xffff) < 0x400)
+         return (void *)&psxH[mem];
+      return NULL;
+   }
+
+   p = (char *)(psxMemWLUT[t]);
+   if (p != NULL)
+      return (void *)(p + (mem & 0xffff));
+   return NULL;
 }
